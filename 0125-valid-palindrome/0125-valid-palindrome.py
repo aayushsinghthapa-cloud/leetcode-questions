7a9b1afdb7bfc5fd191df30ad16f8a0baed083b1
Aayush Singh Thapa <aayushsinghthapa@gmail.com>
2026-09-11 22:43:33 +0530

    Time: 19 ms (47.21%), Space: 12.6 MB (75.81%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,5 +1,5 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
+class Solution(object):
+    def isPalindrome(self, s):
         left, right = 0, len(s) - 1
 
         while left < right:
