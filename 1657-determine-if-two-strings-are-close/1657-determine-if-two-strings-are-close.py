dbe24e0e09cd58159847acaa1b89869236da4b59
Aayush Singh Thapa <aayushsinghthapa@gmail.com>
2026-09-20 22:11:53 +0530

    Time: 155 ms (38.17%), Space: 20.4 MB (69.05%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -1,6 +1,5 @@
-class Solution(object):
-    def closeStrings(self, word1, word2):
-
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
         if len(word1) != len(word2):
             return False
 
@@ -9,9 +8,9 @@ class Solution(object):
 
         for c in word1:
             freq1[c] = freq1.get(c, 0) + 1
-
+        
         for c in word2:
             freq2[c] = freq2.get(c, 0) + 1
-
+        
         return (set(freq1.keys()) == set(freq2.keys()) and
                 sorted(freq1.values()) == sorted(freq2.values()))
